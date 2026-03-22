97565e02f9397bdc9351d29852eae9965d62519a
Guang Qu <qguang95@gmail.com>
2026-03-22 19:11:24 -0400

    Built skills section with animation & contact me section

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { BiMap } from "react-icons/bi";
+import { GrGithub } from "react-icons/gr";
+import { LiaLinkedin } from "react-icons/lia";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="pt-16 pb-16">
+      {/**Contact Me Left Side Info */}
+      <div className="w-[90%] md:w-[80%] lg:w-[70%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center">
+        <div>
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-200">
+            Contact Me
+          </h1>
+          <p className="text-gray-400 mt-6 text-base sm:text-lg">
+            You can reach me using this form! I will respond as promptly as
+            possible.
+          </p>
+          <div className="mt-7">
+            <div className="flex items-center space-x-3 mb-4">
+              <BiMap className="w-9 h-9 text-cyan-300" />
+              <p className="text-xl font-bold text-gray-400">United States</p>
+            </div>
+          </div>
+
+          {/*Outside links */}
+          <div className="flex items-center mt-8 space-x-3">
+            <div className="w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-blue-800 transition-all duration-300">
+              <a href="https://www.linkedin.com/in/guang-qu/" target="_blank">
+                <LiaLinkedin className="text-white w-10 h-10 " />
+              </a>
+            </div>
+            <div className="w-14 h-14 bg-blue-950/60 rounded-full flex items-center justify-center cursor-pointer flex-col hover:bg-blue-800 transition-all duration-300">
+              <a href="https://github.com/guang-qu" target="_blank">
+                <GrGithub className="text-white w-10 h-10 " />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/*Contact Form*/}
+        <div className="md:p-10 p-5 bg-[#131332] rounded-lg">
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="px-4 py-3.5 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70"
+          ></input>
+          <input
+            type="email"
+            placeholder="example@email.com"
+            className="mt-6 px-4 py-3.5 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70"
+          ></input>
+          <textarea
+            placeholder="Write your message here"
+            className="mt-6 px-4 py-3.5 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70 h-40"
+          ></textarea>
+          <button className="mt-6 px-12 py-4 bg-blue-950 hover:bg-blue-900 transition-all duration-300 cursor-pointer text-white rounded-full">Send Message</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -1,7 +1,67 @@
+"use client";
 import React from "react";
+import { FaJava } from "react-icons/fa";
+import {
+  SiCss,
+  SiHtml5,
+  SiJavascript,
+  SiNextdotjs,
+  SiReact,
+  SiTypescript,
+} from "react-icons/si";
+import Tilt from "react-parallax-tilt";
+
+const skills = [
+  {
+    name: "JavaScript",
+    icon: <SiJavascript />,
+  },
+  {
+    name: "React JS",
+    icon: <SiReact />,
+  },
+  {
+    name: "Next.js",
+    icon: <SiNextdotjs />,
+  },
+  {
+    name: "HTML",
+    icon: <SiHtml5 />,
+  },
+  {
+    name: "CSS",
+    icon: <SiCss />,
+  },
+  {
+    name: "TypeScript",
+    icon: <SiTypescript />,
+  },
+  {
+    name: "Java",
+    icon: <FaJava />,
+  },
+];
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="text-white pt-16 pb-16">
+      <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font bold text-white">
+        My <span className="text-cyan-300">Skill Set</span>
+      </h1>
+      <div className="flex flex-wrap justify-center gap-6 mt-16">
+        {skills.map((skill) => {
+          return (
+            <Tilt key={skill.name} scale={1.5} transitionSpeed={400}>
+              <div className="bg-[14134145] text-center w-40 h-48 rounded-3xp flex flex-col items-center justify-center shadow-lg transition hover:scale-105">
+                <div className="text-5xl mb-4 text-gray-300">{skill.icon}</div>
+                <p className="text-purple-400 mt-1">{skill.name}</p>
+              </div>
+            </Tilt>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
