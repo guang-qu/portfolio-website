cbd8d8d01f695da11aa1c2c1abf0e5eeac264be4
Guang Qu <qguang95@gmail.com>
2026-03-31 23:18:57 -0400

    Added project details with image and link

--- components/Home/Projects.tsx/Projects.tsx
+++ components/Home/Projects.tsx/Projects.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+
 
 const Projects = () => {
   return (
@@ -10,14 +12,14 @@ const Projects = () => {
       <div className="w-[70%] mx-auto grid gril-cols-1 md:grid-cols-2 gap-10 mt-16">
         <div>
           <Image
-            src=""
-            alt=""
+            src='/images/HomePage.jpeg'
+            alt="Home page of portfolio website"
             width={800}
             height={650}
             className="rounded-lg"
           />
-          <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white"></h1>
-          <h1 className="pt-2 font-medium text-white/80"></h1>
+          <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">Modern Portfolio Website | <a href="https://github.com/guang-qu/portfolio-website" className="text-cyan-200">GitHub Link</a></h1>
+          <h1 className="pt-2 font-medium text-white/80">Front-End Visuals, API Integration</h1>
         </div>
       </div>
     </div>
